3571e2ccacdf5c2248e2ce955aa6bf3f8a7ae627
RioBoy <boyrio68@gmail.com>
2022-01-10 22:06:22 +0700

    initial commit and finished view admin dashboard

---
+++ routes/admin.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const adminController = require('../controllers/adminContoller');
+
+router.get('/dashboard', adminController.viewDashboard);
+router.get('/category', adminController.viewCategory);
+router.get('/bank', adminController.viewBank);
+router.get('/item', adminController.viewItem);
+router.get('/booking', adminController.viewBooking);
+
+module.exports = router;

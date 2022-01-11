520be78f285b6fc3ad659033e3e2f638877ae960
RioBoy <boyrio68@gmail.com>
2022-01-11 21:35:30 +0700

    CRUD Project Staycation => Category

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const methodOverride = require('method-override');
+
+// import mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/db_bwamern', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +24,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const adminController = require('../controllers/adminContoller');
 
 router.get('/dashboard', adminController.viewDashboard);
+// endpoint category
 router.get('/category', adminController.viewCategory);
+router.post('/category', adminController.addCategory);
+router.put('/category', adminController.editcategory);
+router.delete('/category/:id', adminController.deletecategory);
 router.get('/bank', adminController.viewBank);
 router.get('/item', adminController.viewItem);
 router.get('/booking', adminController.viewBooking);

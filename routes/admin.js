bfa2839555ac2c8ebce91d1d62a8fe0ab0061f64
RioBoy <boyrio68@gmail.com>
2022-01-14 04:56:28 +0700

    CRUD Project Staycation => Bank

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const adminController = require('../controllers/adminContoller');
+const { upload } = require('../middleware/multer');
 
 router.get('/dashboard', adminController.viewDashboard);
 // endpoint category
@@ -7,7 +8,11 @@ router.get('/category', adminController.viewCategory);
 router.post('/category', adminController.addCategory);
 router.put('/category', adminController.editcategory);
 router.delete('/category/:id', adminController.deletecategory);
+// endpoint bank
 router.get('/bank', adminController.viewBank);
+router.post('/bank', upload, adminController.addBank);
+router.put('/bank', upload, adminController.editBank);
+router.delete('/bank/:id', upload, adminController.deleteBank);
 router.get('/item', adminController.viewItem);
 router.get('/booking', adminController.viewBooking);
 

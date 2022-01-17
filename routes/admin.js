85bf3035bc2d288fe02c9d0089f70464134f95fa
RioBoy <boyrio68@gmail.com>
2022-01-18 00:31:40 +0700

    CRUD Project Staycation => Feature

--- routes/admin.js
+++ routes/admin.js
@@ -8,18 +8,27 @@ router.get('/category', adminController.viewCategory);
 router.post('/category', adminController.addCategory);
 router.put('/category', adminController.editcategory);
 router.delete('/category/:id', adminController.deletecategory);
+
 // endpoint bank
 router.get('/bank', adminController.viewBank);
 router.post('/bank', upload, adminController.addBank);
 router.put('/bank', upload, adminController.editBank);
 router.delete('/bank/:id', upload, adminController.deleteBank);
-// endpoint bank
+
+// endpoint item
 router.get('/item', adminController.viewItem);
 router.post('/item', uploadMultiple, adminController.addItem);
 router.get('/item/show-image/:id', adminController.showImageItem);
 router.get('/item/:id', adminController.showEditItem);
 router.put('/item/:id', uploadMultiple, adminController.editItem);
 router.delete('/item/:id/delete', adminController.deleteItem);
+
+// endpoint detail item
+router.get('/item/show-detail-item/:itemId', adminController.viewDetailItem);
+router.post('/item/add/feature', upload, adminController.addfeature);
+router.put('/item/update/feature', upload, adminController.editFeature);
+router.delete('/item/:itemId/feature/:id', adminController.deleteFeature);
+
 router.get('/booking', adminController.viewBooking);
 
 module.exports = router;
